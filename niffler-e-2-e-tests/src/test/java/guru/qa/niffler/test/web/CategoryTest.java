0a6e42d931e93eade5272d145fd6d45637da397e
tmaksyutov <dadojkeee@mail.ru>
2025-10-11 23:36:26 +0500

    добавил тесты по архивации категорий

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/CategoryTest.java
@@ -0,0 +1,58 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.annotation.WebTest;
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.page.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@WebTest
+public class CategoryTest {
+
+    private static final Config CFG = Config.getInstance();
+    private LoginPage loginPage;
+
+    @BeforeEach
+    void setUp() {
+        loginPage = Selenide.open(CFG.frontUrl(), LoginPage.class);
+    }
+
+    @Category(username = "tmaksyutov")
+    @Test
+    void activeCategoryShouldPresentInCategoryList(CategoryJson category) {
+        loginPage
+                .login("tmaksyutov", "12345")
+                .goToProfilePage()
+                .checkProfilePageIsLoaded()
+                .checkCategoryIsDisplayed(category.name());
+    }
+
+    @Category(
+            username = "tmaksyutov",
+            archived = true
+    )
+    @Test
+    void archivedCategoryShouldNotBePresentedInActiveCategoryList(CategoryJson category) {
+        loginPage
+                .login("tmaksyutov", "12345")
+                .goToProfilePage()
+                .checkProfilePageIsLoaded()
+                .checkCategoryIsNotDisplayed(category.name());
+    }
+
+    @Category(
+            username = "tmaksyutov",
+            archived = true
+    )
+    @Test
+    void archivedCategoryShouldBePresentedInArchivedList(CategoryJson category) {
+        loginPage
+                .login("tmaksyutov", "12345")
+                .goToProfilePage()
+                .checkProfilePageIsLoaded()
+                .checkArchivedCategoryExists(category.name());
+    }
+}
